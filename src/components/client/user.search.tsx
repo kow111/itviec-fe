50f22d496324756ad0cab3e9425cc0aa25cb5ed6
kow111 <chome710@gmail.com>
2025-05-20 16:54:35 +0700

    fix ui

--- src/components/client/user.search.tsx
+++ src/components/client/user.search.tsx
@@ -29,61 +29,63 @@ const SearchClient = (props: IProps) => {
   };
 
   return (
-    <Form
-      form={form}
-      onFinish={onFinish}
-      initialValues={{
-        skills: defaultSkills,
-        location: defaultLocations,
-      }}
-    >
-      <Row gutter={[20, 20]}>
-        <Col span={24}>
-          <h2 className="text-2xl font-bold">
-            Việc Làm IT Cho Developer "Chất"
-          </h2>
-        </Col>
-        <Col span={24} md={16}>
-          <Form.Item name="skills">
-            <Select
-              mode="multiple"
-              allowClear
-              showArrow={false}
-              style={{ width: "100%" }}
-              placeholder={
-                <>
-                  <MonitorOutlined /> Tìm theo kỹ năng...
-                </>
-              }
-              optionLabelProp="label"
-              options={optionsSkills}
-            />
-          </Form.Item>
-        </Col>
-        <Col span={12} md={4}>
-          <Form.Item name="location">
-            <Select
-              mode="multiple"
-              allowClear
-              showArrow={false}
-              style={{ width: "100%" }}
-              placeholder={
-                <>
-                  <EnvironmentOutlined /> Địa điểm...
-                </>
-              }
-              optionLabelProp="label"
-              options={optionsLocations}
-            />
-          </Form.Item>
-        </Col>
-        <Col span={12} md={4}>
-          <Button type="primary" onClick={() => form.submit()}>
-            Search
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+    <div className="p-4 ">
+      <Form
+        form={form}
+        onFinish={onFinish}
+        initialValues={{
+          skills: defaultSkills,
+          location: defaultLocations,
+        }}
+      >
+        <Row gutter={[20, 20]}>
+          <Col span={24}>
+            <h2 className="text-2xl font-bold">
+              Việc Làm IT Cho Developer "Chất"
+            </h2>
+          </Col>
+          <Col span={24} md={16}>
+            <Form.Item name="skills">
+              <Select
+                mode="multiple"
+                allowClear
+                showArrow={false}
+                style={{ width: "100%" }}
+                placeholder={
+                  <>
+                    <MonitorOutlined /> Tìm theo kỹ năng...
+                  </>
+                }
+                optionLabelProp="label"
+                options={optionsSkills}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12} md={4}>
+            <Form.Item name="location">
+              <Select
+                mode="multiple"
+                allowClear
+                showArrow={false}
+                style={{ width: "100%" }}
+                placeholder={
+                  <>
+                    <EnvironmentOutlined /> Địa điểm...
+                  </>
+                }
+                optionLabelProp="label"
+                options={optionsLocations}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12} md={4}>
+            <Button type="primary" onClick={() => form.submit()}>
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </div>
   );
 };
 export default SearchClient;
